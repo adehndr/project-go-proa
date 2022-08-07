e4fdc48e4eebe88dc27c447a70bc5b3f6c17a098
adehndr <56023937+adehndr@users.noreply.github.com>
2022-08-07 21:48:45 +0700

    change 8 to heroku

--- app/router.go
+++ app/router.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"fmt"
+	"net/http"
+
 	"example.com/adehndr/project_go_proa/controller"
 	"github.com/julienschmidt/httprouter"
 )
@@ -10,11 +13,14 @@ func NewRouter(taskController controller.TaskController) *httprouter.Router {
 	router.GET("/", taskController.Home)
 	router.GET("/detail", taskController.Detail)
 	router.POST("/detail", taskController.Detail)
-	router.GET("/change/:taskid",taskController.Change)
+	router.GET("/change/:taskid", taskController.Change)
 	router.GET("/api/tasks", taskController.FindAll)
 	router.GET("/api/task/:taskid", taskController.FindById)
 	router.POST("/api/tasks", taskController.Create)
 	router.PUT("/api/task/:taskid", taskController.Update)
 	router.DELETE("/api/task/:taskid", taskController.Delete)
+	router.GET("/test", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w,"Test Success")
+	})
 	return router
 }
